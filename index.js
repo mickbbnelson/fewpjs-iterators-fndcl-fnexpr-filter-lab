a76b74332f600ff91fc87aa1dee1f93c0c93f4ed
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-12 18:03:38 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {  
+    let drivers = array.filter(name => name.toLowerCase() === string.toLowerCase());
+    return drivers
+}
+
+function fuzzyMatch(array, string) {
+    let matches = array.filter(name => {
+        return name.toLowerCase().indexOf(string.toLowerCase()) === 0
+    })
+    return matches
+}
+
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+function matchName(array, string) {
+    let drivers = array.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+    return drivers
+}
